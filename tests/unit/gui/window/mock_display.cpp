97366cd1b60875fb69c3f647a640680199300215
RadekVana <r.v.mail@centrum.cz>
2021-01-25 11:22:17 +0100

    basic rectangle draw tests

--- tests/unit/gui/window/mock_display.cpp
+++ tests/unit/gui/window/mock_display.cpp
@@ -21,17 +21,25 @@ void MockDisplay::clear(color_t clr) {
 }
 
 uint32_t MockDisplay::GetpixelNativeColor(uint16_t point_x, uint16_t point_y) {
-    return pixels[point_x][point_y];
+    return pixels[point_y][point_x];
 }
 
 void MockDisplay::SetpixelNativeColor(uint16_t point_x, uint16_t point_y, uint32_t clr) {
-    pixels[point_x][point_y] = clr;
+    pixels[point_y][point_x] = clr;
 }
 
 uint8_t *MockDisplay::GetBlock(uint16_t start_x, uint16_t start_y, uint16_t end_x, uint16_t end_y) const {
     return nullptr;
 }
-void MockDisplay::FillRectNativeColor(uint16_t rect_x, uint16_t rect_y, uint16_t rect_w, uint16_t rect_h, uint32_t nativeclr) {}
-void MockDisplay::ClrBufferLine(int line) {}
 
-void MockDisplay::drawCharFromBuff(point_ui16_t pt, uint16_t w, uint16_t h) {}
+void MockDisplay::FillRectNativeColor(uint16_t rect_x, uint16_t rect_y, uint16_t rect_w, uint16_t rect_h, uint32_t nativeclr) {
+    for (size_t row = 0; row < rect_h; ++row) {
+        for (size_t col = 0; col < rect_w; ++col) {
+            SetpixelNativeColor(rect_x + col, rect_y + row, nativeclr);
+        }
+    }
+}
+
+//physical draw on display ... does not do anything in virtual one
+void MockDisplay::drawCharFromBuff(point_ui16_t pt, uint16_t w, uint16_t h) {
+}
--- tests/unit/gui/window/tests_layout.cpp
+++ tests/unit/gui/window/tests_layout.cpp
@@ -4,18 +4,68 @@
 #include "ScreenHandler.hpp"
 #include "cmsis_os.h" //HAL_GetTick
 #include "mock_windows.hpp"
+#include "mock_display.hpp"
 #include <memory>
 
 //stubbed header does not have C linkage .. to be simpler
 static uint32_t hal_tick = 0;
 uint32_t HAL_GetTick() { return hal_tick; }
 
-TEST_CASE("Window registration tests", "[window]") {
+static constexpr Rect16 DispRect = { 0, 0, MockDisplay::Cols(), MockDisplay::Rows() };
 
-    REQUIRE(true);
-
-    SECTION("xxx") {
-
-        REQUIRE(true);
+static void TestRectColor(Rect16 rect, color_t color) {
+    for (int16_t X = rect.Left(); X < rect.Width(); ++X) {
+        for (int16_t Y = rect.Top(); Y < rect.Height(); ++Y) {
+            REQUIRE(MockDisplay::Instance().GetpixelNativeColor(X, Y) == color);
+        }
     }
-}
+};
+
+//Minuend: The number that is to be subtracted from.
+//Subtrahend: The number that is to be subtracted.
+static void TestRectDiffColor(Rect16 Minuend, Rect16 Subtrahend, color_t MinuendColor, color_t DiffColor) {
+    for (int16_t X = Minuend.Left(); X < Minuend.Width(); ++X) {
+        for (int16_t Y = Minuend.Top(); Y < Minuend.Height(); ++Y) {
+            if (Subtrahend.Contain(point_i16_t({ X, Y }))) {
+                REQUIRE(MockDisplay::Instance().GetpixelNativeColor(X, Y) == DiffColor);
+            } else {
+                REQUIRE(MockDisplay::Instance().GetpixelNativeColor(X, Y) == MinuendColor);
+            }
+        }
+    }
+};
+
+static void TestRectDraw(Rect16 rect, color_t color) {
+    window_t win(nullptr, rect);
+    win.SetBackColor(color);
+    win.Draw();
+    TestRectColor(rect, color);
+};
+
+static void TestDispRectDraw(Rect16 rect, color_t color_win, color_t color_disp) {
+    MockDisplay::Instance().clear(color_disp);
+    TestRectColor(DispRect, color_disp);
+    TestRectDraw(rect, color_win);
+    TestRectDiffColor(DispRect, rect, color_disp, color_win);
+};
+
+TEST_CASE("Window layout tests", "[window]") {
+    MockDisplay::Instance().clear(COLOR_BLACK);
+    TestRectColor(DispRect, COLOR_BLACK);
+
+    SECTION("RECT") {
+        TestDispRectDraw(Rect16(0, 0, 0, 0), COLOR_BLUE, COLOR_WHITE);
+
+        TestDispRectDraw(DispRect, COLOR_BLUE, COLOR_WHITE);
+
+        TestDispRectDraw({ 1, 0, MockDisplay::Cols() - 1, MockDisplay::Rows() }, COLOR_RED, COLOR_BLACK);
+
+        TestDispRectDraw(Rect16(10, 20, 1, 2), COLOR_BLUE, COLOR_WHITE);
+
+        TestDispRectDraw(Rect16(0, 0, 1, 2), COLOR_RED, COLOR_BLUE);
+
+        TestDispRectDraw(Rect16(10, 20, 1, 2), COLOR_WHITE, COLOR_BLACK);
+
+        TestDispRectDraw(Rect16(10, 20, 1, 2), COLOR_BLACK, COLOR_BLUE);
+    }
+};
